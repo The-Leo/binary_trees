9455af2c511ca4a4ecb696b8b26ce549621e010c
Leonard Odoi <odoi.leonard1@gmail.com>
2023-08-31 14:01:46 -0700

    Include header file

---
+++ binary_trees.h
@@ -0,0 +1,14 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+
+
+
+#endif /* BINARY_TREES_H */
