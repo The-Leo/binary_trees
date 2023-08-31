2d49ada6c460a189edb2c51e629f744211eea3b0
Leonard Odoi <odoi.leonard1@gmail.com>
2023-08-31 14:04:04 -0700

    Implement structures

--- binary_trees.h
+++ binary_trees.h
@@ -9,6 +9,23 @@
 #include <limits.h>
 
 
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
 
+typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
+typedef struct binary_tree_s heap_t;
 
 #endif /* BINARY_TREES_H */
